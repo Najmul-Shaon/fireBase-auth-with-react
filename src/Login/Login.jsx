b033a2e1109f9938ac347174f4a7b03734357580
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-05-13 21:22:53 +0600

    signout btn and toggle with sign in btn

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,23 +1,58 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../Firebase/Firebase.init";
+import { useState } from "react";
 const Login = () => {
+  const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
       })
       .catch((error) => {
         console.log("Error", error.message);
       });
   };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
-      <button onClick={handleGoogleSignIn} className="btn">
-        Google Login
-      </button>
+      {/* sign in and sign out button will be toggle here. */}
+      {user ? (
+        <button onClick={handleGoogleSignOut} className="btn">
+          Google Log Out
+        </button>
+      ) : (
+        <button onClick={handleGoogleSignIn} className="btn">
+          Google Login
+        </button>
+      )}
+
+      {user && (
+        <div>
+          <h3>User:{user?.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+          <p>phone: {user.phoneNumber}</p>
+        </div>
+      )}
     </div>
   );
 };
